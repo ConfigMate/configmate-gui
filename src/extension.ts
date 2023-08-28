fc4744ab6dfbc09a6983ba132087504ea4e9e220
KT Minckler <katie.minckler@gmail.com>
2023-08-28 13:56:09 -0400

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -2,35 +2,44 @@
 
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	const writeEmitter = new vscode.EventEmitter<string>();
-	context.subscriptions.push(vscode.commands.registerCommand('cliFileParser.create', () => {
-		let line = '';
-		const pty = {
-			onDidWrite: writeEmitter.event,
-			open: () => writeEmitter.fire('Type and press enter to echo the text\r\n\r\n'),
-			close: () => { /* noop*/ },
-			handleInput: (data: string) => {
-				if (data === '\r') { // Enter
-					writeEmitter.fire(`\r\necho: "${colorText(line)}"\r\n\n`);
-					line = '';
-					return;
-				}
-				if (data === '\x7f') { // Backspace
-					if (line.length === 0) {
-						return;
-					}
-					line = line.substr(0, line.length - 1);
-					// Move cursor backward
-					writeEmitter.fire('\x1b[D');
-					// Delete character
-					writeEmitter.fire('\x1b[P');
-					return;
-				}
-				line += data;
-				writeEmitter.fire(data);
+// Define the Pseudoterminal
+// https://code.visualstudio.com/api/references/vscode-api#Pseudoterminal
+
+let input = '';
+const writeEmitter = new vscode.EventEmitter<string>();
+const pty = {
+	onDidWrite: writeEmitter.event,
+	open: () => writeEmitter.fire(getMenu()),
+	close: () => { /* noop*/ },
+	handleInput: (keystroke: string) => {
+		switch (keystroke) {
+			case '\r': { // Enter
+				/* Handle user input here
+				Pass input to other functions defined outside of activate()
+				Currently, just echoes the input colorfully */
+
+				writeEmitter.fire(`\r\necho: "${colorText(input)}"\r\n\n`);
+				input = '';
+				return;
 			}
-		};
+			case '\x7f': { // Backspace
+				if (input.length === 0) return;
+				input = input.substr(0, input.length - 1);
+				// Move cursor backward
+				writeEmitter.fire('\x1b[D');
+				// Delete character
+				writeEmitter.fire('\x1b[P');
+				return;
+			}
+		}
+		input += keystroke;
+		writeEmitter.fire(keystroke);
+	}
+};
+
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('cliFileParser.create', () => {
 		const terminal = vscode.window.createTerminal({ name: `CLI File Parser`, pty });
 		terminal.show();
 	}));
@@ -41,18 +50,36 @@ export function activate(context: vscode.ExtensionContext) {
 
 }
 
+function parseMenu(choice: number, input: string): string {
+	switch (choice) {
+		case 1: 
+			return colorText(input);
+		case 2: 
+			return `Fetching the file you requested: ${input}\r\n`;
+	}
+	return 'Something went wrong parsing your menu choice!\r\n';
+}
+
+function getMenu(): string {
+	const menu = `
+*********************************
+CHOOSE FROM THE FOLLOWING OPTIONS
+	1. Color my text
+	2. Fetch a file
+	3. Exit
+`;
+	return menu;
+}
+
 function colorText(text: string): string {
 	let output = '';
 	let colorIndex = 1;
 	for (let i = 0; i < text.length; i++) {
 		const char = text.charAt(i);
-		if (char === ' ' || char === '\r' || char === '\n') {
-			output += char;
-		} else {
+		if (char === ' ' || char === '\r' || char === '\n') output += char;
+		else {
 			output += `\x1b[3${colorIndex++}m${text.charAt(i)}\x1b[0m`;
-			if (colorIndex > 6) {
-				colorIndex = 1;
-			}
+			if (colorIndex > 6) colorIndex = 1;
 		}
 	}
 	return output;
