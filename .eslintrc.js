5bafbabea801c624cf64285850f4333a092b8bff
KT Minckler <katie.minckler@gmail.com>
2023-09-18 15:43:40 -0400

    linting & documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 	plugins: [
 		'@typescript-eslint',
 	],
